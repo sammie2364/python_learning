f15de514bfac99d035486718a6a424ae1137c65d
Samuel Karau Njenga <rizzymanadriller@gmail.com>
2026-01-17 12:54:24 +0300

    Reorganize project into lesson-based folders

---
+++ lesson2/Advanced_personal _info.py
@@ -0,0 +1,20 @@
+name=input("enter your name")
+age=int(input("enter your age"))
+programming_language=input("your favourite programming language")
+version_control=input("enter your version control")
+
+print(f""" 
+{"#"*65}
+       Advanced Personal Info
+{"#"*65}
+
+name: {name}
+age: {age}
+fav_language: {programming_language}
+i use: {version_control}
+
+{"#"*65}
+
+
+
+""")
